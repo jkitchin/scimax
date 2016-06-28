dbff5069d7a505c1c8d24eb1c1a4d25dfc0fcffe
John Kitchin <jkitchin@andrew.cmu.edu>
2016-06-27 21:25:21 -0400

    Revert "strip out unneeded parts of script"
    This reverts commit 47c7600c28ca537c519abbda94b09e2eac78f5e4.

--- ox-clip.el
+++ ox-clip.el
@@ -29,6 +29,39 @@ __version__ = \"0.1\"
 import re
 import win32clipboard
 
+#---------------------------------------------------------------------------
+#  Convenience functions to do the most common operation
+
+def HasHtml():
+    \"\"\"
+    Return True if there is a Html fragment in the clipboard..
+    \"\"\"
+    cb = HtmlClipboard()
+    return cb.HasHtmlFormat()
+
+
+def GetHtml():
+    \"\"\"
+    Return the Html fragment from the clipboard or None if there is no Html in the clipboard.
+    \"\"\"
+    cb = HtmlClipboard()
+    if cb.HasHtmlFormat():
+        return cb.GetFragment()
+    else:
+        return None
+
+
+def PutHtml(fragment):
+    \"\"\"
+    Put the given fragment into the clipboard.
+    Convenience function to do the most common operation
+    \"\"\"
+    cb = HtmlClipboard()
+    cb.PutFragment(fragment)
+
+
+#---------------------------------------------------------------------------
+
 class HtmlClipboard:
 
     CF_HTML = None
@@ -85,6 +118,105 @@ class HtmlClipboard:
         return self.CF_HTML
 
 
+    def GetAvailableFormats(self):
+        \"\"\"
+        Return a possibly empty list of formats available on the clipboard
+        \"\"\"
+        formats = []
+        try:
+            win32clipboard.OpenClipboard(0)
+            cf = win32clipboard.EnumClipboardFormats(0)
+            while (cf != 0):
+                formats.append(cf)
+                cf = win32clipboard.EnumClipboardFormats(cf)
+        finally:
+            win32clipboard.CloseClipboard()
+
+        return formats
+
+
+    def HasHtmlFormat(self):
+        \"\"\"
+        Return a boolean indicating if the clipboard has data in HTML format
+        \"\"\"
+        return (self.GetCfHtml() in self.GetAvailableFormats())
+
+
+    def GetFromClipboard(self):
+        \"\"\"
+        Read and decode the HTML from the clipboard
+        \"\"\"
+
+        try:
+            win32clipboard.OpenClipboard(0)
+            src = win32clipboard.GetClipboardData(self.GetCfHtml())
+            #print src
+            self.DecodeClipboardSource(src)
+        finally:
+            win32clipboard.CloseClipboard()
+
+
+    def DecodeClipboardSource(self, src):
+        \"\"\"
+        Decode the given string to figure out the details of the HTML that's on the string
+        \"\"\"
+                    # Try the extended format first (which has an explicit selection)
+        matches = self.MARKER_BLOCK_EX_RE.match(src)
+        if matches:
+            self.prefix = matches.group(0)
+            self.htmlClipboardVersion = matches.group(1)
+            self.html = src[int(matches.group(2)):int(matches.group(3))]
+            self.fragment = src[int(matches.group(4)):int(matches.group(5))]
+            self.selection = src[int(matches.group(6)):int(matches.group(7))]
+            self.source = matches.group(8)
+        else:
+                    # Failing that, try the version without a selection
+            matches = self.MARKER_BLOCK_RE.match(src)
+            if matches:
+                self.prefix = matches.group(0)
+                self.htmlClipboardVersion = matches.group(1)
+                self.html = src[int(matches.group(2)):int(matches.group(3))]
+                self.fragment = src[int(matches.group(4)):int(matches.group(5))]
+                self.source = matches.group(6)
+                self.selection = self.fragment
+
+
+    def GetHtml(self, refresh=False):
+        \"\"\"
+        Return the entire Html document
+        \"\"\"
+        if not self.html or refresh:
+            self.GetFromClipboard()
+        return self.html
+
+
+    def GetFragment(self, refresh=False):
+        \"\"\"
+        Return the Html fragment. A fragment is well-formated HTML enclosing the selected text
+        \"\"\"
+        if not self.fragment or refresh:
+            self.GetFromClipboard()
+        return self.fragment
+
+
+    def GetSelection(self, refresh=False):
+        \"\"\"
+        Return the part of the HTML that was selected. It might not be well-formed.
+        \"\"\"
+        if not self.selection or refresh:
+            self.GetFromClipboard()
+        return self.selection
+
+
+    def GetSource(self, refresh=False):
+        \"\"\"
+        Return the URL of the source of this HTML
+        \"\"\"
+        if not self.selection or refresh:
+            self.GetFromClipboard()
+        return self.source
+
+
     def PutFragment(self, fragment, selection=None, html=None, source=None):
         \"\"\"
         Put the given well-formed fragment of Html into the clipboard.
@@ -136,12 +268,26 @@ class HtmlClipboard:
                         source)
         return (prefix + html)
 
+
+def DumpHtml():
+
+    cb = HtmlClipboard()
+    print \"GetAvailableFormats()=%s\" % str(cb.GetAvailableFormats())
+    print \"HasHtmlFormat()=%s\" % str(cb.HasHtmlFormat())
+    if cb.HasHtmlFormat():
+        cb.GetFromClipboard()
+        print \"prefix=>>>%s<<<END\" % cb.prefix
+        print \"htmlClipboardVersion=>>>%s<<<END\" % cb.htmlClipboardVersion
+        print \"GetSelection()=>>>%s<<<END\" % cb.GetSelection()
+        print \"GetFragment()=>>>%s<<<END\" % cb.GetFragment()
+        print \"GetHtml()=>>>%s<<<END\" % cb.GetHtml()
+        print \"GetSource()=>>>%s<<<END\" % cb.GetSource()
+
+        
 if __name__ == '__main__':
     import sys
     data = sys.stdin.read()
-    cb = HtmlClipboard()
-    cb.PutFragment(data)
-
+    PutHtml(data)
 "
   "Windows Python Script for copying formatted text")
 
