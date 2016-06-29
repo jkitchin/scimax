71cdba0d91b7ac9d43ea6a1faead75f642960cd9
John Kitchin <jkitchin@andrew.cmu.edu>
2016-06-28 21:36:00 -0400

    add copyright

--- ox-clip.el
+++ ox-clip.el
@@ -1,5 +1,24 @@
 ;;; ox-clip.el --- Formatted copy commands across platforms
 
+;; Copyright(C) 2016 John Kitchin
+
+;; This file is not currently part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program ; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
 ;;; Commentary: This module copies selected regions in org-mode as formatted
 ;;; text on the clipboard that can be pasted into other applications.
 
