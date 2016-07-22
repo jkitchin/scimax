93eeeb198909aab11d96a9684134825b6a1a54ec
John Kitchin <jkitchin@andrew.cmu.edu>
2016-07-22 11:02:37 -0400

    add git commits to install script, and make a bat file

--- install-scimax-win.sh
+++ install-scimax-win.sh
@@ -7,18 +7,22 @@ fi
 
 cd scimax
 git submodule add https://github.com/jkitchin/emacs-win
+git submodule add https://github.com/jkitchin/scimax-win-elpa elpa
 git submodule init
 git submodule update
+
+git add emacs-win
+git commit emacs-win -m "add windows emacs"
+git add .gitmodules
+git commit .gitmodules -m "windows setup for submodules"
 
 echo "scimax is installed. To use it, run this command in your terminal."
 echo "`pwd`/scimax/emacs-win/bin/runemacs.exe -q -l `pwd`/scimax/init.el"
 echo "or"
 echo "run the scimax.sh script created in this directory as ./scimax.sh in the terminal."
 
-echo "#!/bin/bash
-`pwd`/scimax/emacs-win/bin/runemacs.exe -q -l `pwd`/scimax/init.el
-#end" > scimax.sh
-chmod +x scimax.sh
+echo "start \"\" \".\scimax\emacs-win\bin\runemacs.exe\" -l \".\scimax\init.el\"" > scimax.bat
+
 
 echo "Opening scimax. The first time it will install a lot of packages. Be patient."
 start "" ".\scimax\emacs-win\bin\runemacs.exe" -l ".\scimax\init.el"
