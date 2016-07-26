ebe0895725547e4e8b0a57a46b86ea7a1eb283ab
John Kitchin <jkitchin@andrew.cmu.edu>
2016-07-26 14:40:33 -0400

    convert sh back to bat with windows path

--- install-scimax-win.sh
+++ install-scimax-win.sh
@@ -19,9 +19,11 @@ git commit .gitmodules -m "windows setup for submodules"
 echo "scimax is installed. To use it, run this command in your terminal."
 echo "`pwd`/scimax/emacs-win/bin/runemacs.exe -q -l `pwd`/scimax/init.el"
 echo "or"
-echo "run the scimax.bat script created in this directory as ./scimax.bat in the terminal."
+echo "run the scimax.bat script created in this directory as ./scimax.sh in the terminal."
 
-echo "start \"\" \"`pwd`/emacs-win/bin/runemacs.exe\" -l \"`pwd`/init.el\" \"\$@\"" > scimax.sh
+# This converts the posix style path from git bash to a windows path.
+SCIMAX_ROOT=$(echo `pwd` | sed -e 's/^\///' -e 's/\//\\/g' -e 's/^./\0:/')
+echo "start \"\" \"${SCIMAX_ROOT}\\emacs-win\\bin\\runemacs.exe\" -l \"${SCIMAX_ROOT}\\init.el\" \"\$@\"" > scimax.bat
 
 
 echo "Opening scimax.  Be patient."
