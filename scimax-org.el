a6538bedac4f1cd0f2f7d8766bbba80348c2d575
John Kitchin <jkitchin@andrew.cmu.edu>
2017-04-12 17:43:56 -0400

    make new return behavior open links and not expand inline tasks.

--- scimax-org.el
+++ scimax-org.el
@@ -1512,7 +1512,17 @@ Use a prefix arg to get regular RET. "
   (if ignore
       (org-return)
     (cond
-     ((org-in-item-p)
+     ;; Open links like usual
+     ((eq 'link (car (org-element-context)))
+      (org-return))
+     ;; It doesn't make sense to add headings in inline tasks. Thanks Anders
+     ;; Johansson!
+     ((org-inlinetask-in-task-p)
+      (org-return))
+     ;; lists end with two blank lines, so we need to make sure we are also not
+     ;; at the beginning of a line to avoid a loop where a new entry gets
+     ;; created with only one blank line.
+     ((and (org-in-item-p) (not (bolp)))
       (if (org-element-property :contents-begin (org-element-context))
 	  (org-insert-heading)
 	(beginning-of-line)
