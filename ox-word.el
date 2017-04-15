81180941f2efb639fe56b5ae4320f6e3b36f1685
John Kitchin <jkitchin@andrew.cmu.edu>
2017-04-15 15:57:09 -0400

    add new module for exporting to word.
    The first method will just use pandoc and Latex.

---
+++ ox-word.el
@@ -0,0 +1,83 @@
+;;; ox-word.el --- Collection of functions to export to MS Word documents
+;; Collection of ideas to get Word documents
+
+;; * Using pandoc via LaTeX
+
+;; Use #+PANDOC-CSL: /Users/jkitchin/Downloads/elsevier-with-titles.csl to set
+;; the bibliography style. You can get the csl files from
+;; https://www.zotero.org/styles
+
+
+;;; Commentary:
+;;
+
+;;; Code:
+
+(defun ox-export-via-latex-pandoc-to-docx-and-open (&optional async subtreep visible-only body-only options)
+  "Export the current org file as a docx via LaTeX."
+  (interactive)
+  (let* ((bibfile (expand-file-name (car (org-ref-find-bibliography))))
+	 (temp-bib)
+	 (bibtex-entries)
+	 biboption
+	 csl
+	 ;; this is probably a full path
+	 (current-file (buffer-file-name))
+	 (basename (file-name-sans-extension current-file))
+	 (tex-file (concat basename ".tex"))
+	 (docx-file (concat basename ".docx")))
+
+    (save-buffer)
+
+    ;; I make a temp bibfile because my big one causes pandoc to choke. This
+    ;; should only create a file with the required entries.
+    (when bibfile
+      (setq bibtex-entries (let* ((bibtex-files (org-ref-find-bibliography))
+				  (keys (reverse (org-ref-get-bibtex-keys)))
+				  (bibtex-entry-kill-ring-max (length keys))
+				  (bibtex-entry-kill-ring '()))
+
+			     (save-window-excursion
+			       (cl-loop for key in keys
+					do
+					(bibtex-search-entry key t)
+					(bibtex-kill-entry t)))
+			     (mapconcat
+			      'identity
+			      bibtex-entry-kill-ring
+			      "\n\n"))
+	    temp-bib (make-temp-file "ox-word-" nil ".bib")
+	    biboption (format " --bibliography=%s " temp-bib))
+      (with-temp-file temp-bib
+	(insert bibtex-entries)))
+
+    (setq csl (cdr (assoc "PANDOC-CSL"
+			  (org-element-map (org-element-parse-buffer) 'keyword
+			    (lambda (key) (cons
+					   (org-element-property :key key)
+					   (org-element-property :value key)))))))
+    (if csl (setq csl (format " --csl=%s " csl))
+      (setq csl " "))
+
+
+    (org-latex-export-to-latex async subtreep visible-only body-only options)
+
+    (when (file-exists-p docx-file) (delete-file docx-file))
+    (shell-command (format
+		    "pandoc -s -S %s%s%s -o %s"
+		    biboption
+		    csl
+		    tex-file
+		    docx-file))
+    (delete-file temp-bib)
+    (org-open-file docx-file '(16))))
+
+
+(org-export-define-derived-backend 'MSWord 'latex
+  :menu-entry
+  '(?w "Export to MS Word"
+       ((?p "via Pandoc/LaTeX" ox-export-via-latex-pandoc-to-docx-and-open))))
+
+(provide 'ox-word)
+
+;;; ox-word.el ends here
