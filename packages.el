ed838db1108f046e5f7762133e2d00a2a27343a4
John Kitchin <jkitchin@andrew.cmu.edu>
2017-08-05 07:38:16 -0400

    fix error

--- packages.el
+++ packages.el
@@ -461,7 +461,7 @@
 
 (use-package ox-clip
   :ensure nil
-  :load-path (expand-file-name "ox-clip" scimax-dir)
+  :load-path (lambda () (expand-file-name "ox-clip" scimax-dir))
   :bind ("H-k" . ox-clip-formatted-copy))
 
 (use-package scimax-email
