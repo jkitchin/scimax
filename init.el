cd901faeeecae1d881699e4f0810ff30049fb0d9
John Kitchin <kitchin@google.com>
2018-02-12 12:00:41 -0800

    YA attempt to fix issue #159
    It seems related to ssl. I don't have a way to update ssl in the emacs win build right now. trying this instead.

--- init.el
+++ init.el
@@ -48,6 +48,16 @@
   (add-to-list
    'package-archives
    (cons "org" (concat proto "://orgmode.org/elpa/")))
+
+  (when no-ssl
+    (setq tls-program
+	  ;; Defaults:
+	  '("gnutls-cli --insecure -p %p %h"
+	    "gnutls-cli --insecure -p %p %h --protocols ssl3"
+	    "openssl s_client -connect %h:%p -no_ssl2 -ign_eof")
+	  ;; '("gnutls-cli -p %p %h"
+	  ;;   "openssl s_client -connect %h:%p -no_ssl2 -no_ssl3 -ign_eof")
+	  ))
   
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
