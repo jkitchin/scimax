1d19c401071e718c0ab0c1e0f2cf8d203dd8e28a
John Kitchin <kitchin@google.com>
2018-05-10 15:31:04 -0700

    additions of new snippets. some reorganization. some new functionalities.

--- scimax-yas.el
+++ scimax-yas.el
@@ -1,11 +1,17 @@
 ;;; scimax-yas.el --- scimax setup for snippets
 
 ;;; Commentary:
+;; This module provides a scimax customized setup for yasnippets. It was
+;; motivated by some recent discussions on the org-mode mailing list about
+;; replacing the <template mechanism with something new. I have been meaning to
+;; figure out how to use snippets, and this module provides a more useful way
+;; (imho) to use them. Notably, you can use `scimax-ivy-yas' to insert a snippet
+;; using completion.
 ;;
+;; The variable `scimax-snippet-dir' defines the directory where scimax snippets
+;; are stored.
 
 (require 'yasnippet)
-
-(setq helm-yas-space-match-any-greedy t)
 
 (defcustom scimax-snippet-dir (expand-file-name "snippets" scimax-dir)
   "Directory to store snippets in.")
@@ -30,15 +36,37 @@ Adapated from helm-c-yasnippet."
 			template))
 		     (yas--all-templates (yas--get-snippet-tables)))))
     (ivy-read "insert: " candidates
-	      :action (lambda (candidate)
-			(let ((yas--current-template (cdr candidate))
-			      (where (if (region-active-p)
-					 (cons (region-beginning) (region-end))
-				       (cons (point) (point)))))
-			  (yas-expand-snippet (yas--template-content yas--current-template)
-					      (car where)
-					      (cdr where)
-					      (yas--template-expand-env yas--current-template)))))))
+	      :action '(1
+			("o"
+			 (lambda (candidate)
+			   (let ((yas--current-template (cdr candidate))
+				 (where (if (region-active-p)
+					    (cons (region-beginning) (region-end))
+					  (cons (point) (point)))))
+			     (yas-expand-snippet (yas--template-content yas--current-template)
+						 (car where)
+						 (cdr where)
+						 (yas--template-expand-env yas--current-template))))
+			 "Expand selected template")
+			
+			("v"
+			 (lambda (candidate)
+			   (find-file (yas--template-get-file (cdr candidate))))
+			 "Visit template file")
+			
+			("d"
+			 (lambda (candidate)
+			   (when (y-or-n-p "Delete?")
+			     (delete-file (yas--template-get-file (cdr candidate)))
+			     (yas-reload-all)))
+			 "Delete snippet")
+			
+			("n"
+			 (lambda (candidate)
+			   (yas-new-snippet))
+			 "new template")
+
+			))))
 
 
 (provide 'scimax-yas)
