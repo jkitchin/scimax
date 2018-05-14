c04436f3695dfd454b24d76e243e205d60766e71
John Kitchin <kitchin@google.com>
2018-05-14 15:50:21 -0700

    add f-string

--- scimax-utils.el
+++ scimax-utils.el
@@ -213,6 +213,32 @@ Returns whatever BODY would return."
 	     (buffer-list)))))
 
 
+;; * f-strings
+
+(defmacro f-string (fmt)
+  "Like `s-format' but with format fields in it.
+FMT is a string to be expanded against the current lexical
+environment. It is like what is used in `s-lex-format', but has
+an expanded syntax to allow format-strings. For example:
+
+${user-full-name 20s} will be expanded to the current value of
+the variable `user-full-name' in a field 20 characters wide.
+
+  (let ((f (sqrt 5)))  (f-string \"${f 1.2f}\"))
+
+  will render as: 2.24
+
+"
+  (let* ((matches (s-match-strings-all"${\\(?3:\\(?1:[^} ]+\\) *\\(?2:[^}]*\\)\\)}" fmt))
+	 (agetter (loop for (m0 m1 m2 m3) in matches
+			collect `(,m3 .  ,(format (format "%%%s" (if (string= m2 "")
+								     (if s-lex-value-as-lisp "S" "s")
+								   m2))
+						  (symbol-value (intern m1)))))))
+    
+    `(s-format ,fmt 'aget ',agetter)))
+
+
 ;; * The end
 (provide 'scimax-utils)
 
