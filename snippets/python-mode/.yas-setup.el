8151e9fd83d2562115f2472a7a25f3f7cf4f9119
John Kitchin <kitchin@google.com>
2018-06-01 16:53:41 -0700

    modify the snippet for numpy docstrings

--- snippets/python-mode/.yas-setup.el
+++ snippets/python-mode/.yas-setup.el
@@ -2,7 +2,8 @@
   (interactive)
   (let (arg-string
 	arg-pairs
-	arg-tuples)
+	arg-tuples
+	results)
     (when (re-search-backward "def .*(\\(.*\\)):" nil t)
       (setq arg-string (match-string-no-properties 1)
 	    arg-pairs (mapcar 's-trim (split-string arg-string ","))
@@ -10,7 +11,11 @@
 				 (mapcar 's-trim
 					 (split-string pair "=")))
 			       arg-pairs))
-      (cl-loop for (arg val) in arg-tuples concat
-	       (if val
-		   (format "%s : default=%s\n\n" arg val)
-		 (format "%s : \n\n" arg))))))
+      (setq arg-tuples (-filter (lambda (el) (not (string= (car el) "self")))
+				arg-tuples))
+      (setq results
+	    (cl-loop for (arg val) in arg-tuples collect
+		     (if val
+			 (format "%s : optional, default=%s" arg val)
+		       (format "%s : " arg))))
+      (s-join "\n\n" results))))
