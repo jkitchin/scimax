1886944d72e25a38984b3a8dec0435fcc59c30ad
John Kitchin <jkitchin@andrew.cmu.edu>
2018-06-03 20:52:03 -0700

    clean up the projectile hydra
    there were some duplicates I removed, and a few key bindings I changed.

--- scimax-hydra.el
+++ scimax-hydra.el
@@ -730,9 +730,9 @@ _C-a_ Async export: %`hydra-ox/async-export
 
      Find File            Search/Tags          Buffers                Cache
 ------------------------------------------------------------------------------------------
-_s-f_: file            _a_: ag                _i_: Ibuffer           _c_: cache clear
- _ff_: file dwim       _g_: update gtags      _b_: switch to buffer  _x_: remove known project
- _fd_: file curr dir   _o_: multi-occur     _s-k_: Kill all buffers  _X_: cleanup non-existing
+ _ff_: file            _a_: ag                _i_: Ibuffer           _c_: cache clear
+ _fw_: file dwim       _g_: update gtags      _b_: switch to buffer  _x_: remove known project
+ _fd_: file curr dir   _o_: multi-occur       _K_: Kill all buffers  _X_: cleanup non-existing
   _r_: recent file                                               ^^^^_z_: cache current
   _d_: dir
 
@@ -741,19 +741,14 @@ _s-f_: file            _a_: ag                _i_: Ibuffer           _c_: cache 
   ("b"   projectile-switch-to-buffer)
   ("c"   projectile-invalidate-cache)
   ("d"   projectile-find-dir)
-  ("s-f" projectile-find-file)
-  ("ff"  projectile-find-file-dwim)
+  ("ff"  projectile-find-file)
+  ("fw"  projectile-find-file-dwim)
   ("fd"  projectile-find-file-in-directory)
   ("g"   ggtags-update-tags)
-  ("s-g" ggtags-update-tags)
   ("i"   projectile-ibuffer)
   ("K"   projectile-kill-buffers)
-  ("s-k" projectile-kill-buffers)
   ("m"   projectile-multi-occur)
-  ("o"   projectile-multi-occur)
-  ("s-p" projectile-switch-project "switch project")
   ("p"   projectile-switch-project)
-  ("s"   projectile-switch-project)
   ("r"   projectile-recentf)
   ("x"   projectile-remove-known-project)
   ("X"   projectile-cleanup-known-projects)
