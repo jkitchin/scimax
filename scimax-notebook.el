cd409794d01cde0dee2bac9120806d0463244e17
John Kitchin <jkitchin@andrew.cmu.edu>
2018-06-03 17:33:06 -0700

    Add a feature to compute the master nb file.
    Inspired by issue #212.
    - change defvars to defcustoms.
    - improve some docstrings.
    - change defaults on the menu. This may have been a slow thing.

--- scimax-notebook.el
+++ scimax-notebook.el
@@ -1,40 +1,76 @@
 ;;; scimax-notebook.el ---    -*- lexical-binding: t -*-
-;;; Header:
 
 ;;; Commentary:
 ;; This is an experiment in using scimax and org-mode for scientific notebook
 ;; purposes. The idea is you have a "project" that is a set of org and other
-;; files under version control (git). You can use `projectile' to switch between
-;; projects easily, or search/find files within a project.
+;; files under version control (git). There is a "master" file that is the
+;; starting point, e.g. the README.org file. You can use `projectile' to switch
+;; between projects easily, or search/find files within a project.
 ;;
-;; This code sets up a notebook agenda command `nb-agenda` to see the TODO items
-;; within a project, or do other org-agenda things within the scope of the
-;; project, e.g. search by tag/property.
+;; `nb-new' is command to create a new project, it is just a thin wrapper that
+;; creates the directories, registers them with projectile, and opens the master
+;; file.
+;;
+;; `nb-open' is a command to open an existing project. It is a thin wrapper
+;; around the projectile-switch-project command that opens the master file.
+;;
+;; `nb-agenda' to see the TODO items within a project, or do other org-agenda
+;; things within the scope of the project, e.g. search by tag/property.
+;;
+;; `nb-archive' creates a zip-archive of the project.
 
 ;;; Code:
 ;; * Setup
 (projectile-mode +1)
 
 
-(defvar nb-notebook-directory
+(defcustom nb-notebook-directory
   "~/vc/projects/"
-  "Directory where projects are stored.")
+  "Directory where projects are stored."
+  :group 'scimax-notebook
+  :type '(directory))
 
 
 (unless (file-directory-p nb-notebook-directory)
   (make-directory nb-notebook-directory t))
 
 
-(defvar nb-master-file "README.org"
-  "Name of the master file in each project.")
+(defcustom nb-master-file (lambda (&optional name)
+			    "Return the master file name for the project."
+			    "README.org")
+  "A function that returns the master file in each project.
+The function must take one optional argument that is a project
+name. This function will be run in the root directory of the
+project. The function should return a string of the master file
+name. See `nb-example-master' for an example of a computed maste
+file."
+  :group 'scimax-notebook)
+
+
+(defun nb-example-master (&optional name)
+  "Return the master filename for the project of NAME.
+NAME is optional, and if it is nil, compute the filename from the
+current directory. This master file is an org-file with the name
+of the root directory, with a @ prefix so it sorts to the top of
+the directory with ls."
+  (concat "@"
+	  (file-name-base (directory-file-name default-directory))
+	  ".org"))
+
+
+(defcustom nb-scimax-update-menu-p nil
+  "If non-nil, add a hook that updates the menu with known projects.
+This can be slow if you have a lot of projects because it runs
+when in a menu bar update hook. If nil, just add projects once.")
 
 
 ;;;###autoload
 (defun nb-open ()
-  "Switch to a project and open the main file."
+  "Switch to a project and open the main file.
+This is a thin wrapper on `projectile-switch-project' that opens the master file."
   (interactive)
   (let ((projectile-switch-project-action (lambda ()
-					    (find-file nb-master-file)))) 
+					    (find-file (read-file-name "File: " "." nil nil (funcall nb-master-file))))))
     (projectile-switch-project)))
 
 
@@ -42,15 +78,15 @@
 (defun nb-new (name)
   "Create a new project of NAME in `nb-notebook-directory'."
   (interactive "sNew project name: ")
-  (let ((dir (file-name-as-directory (expand-file-name name nb-notebook-directory))))
+  (let ((dir (file-name-as-directory (expand-file-name name nb-notebook-directory)))
+	(nb-master-file-name (funcall nb-master-file name)))
     (unless (file-directory-p dir)
       (make-directory dir t)
       (let ((default-directory dir))
 	(shell-command "git init")))
     (projectile-add-known-project dir)
     (projectile-save-known-projects)
-    (find-file (expand-file-name nb-master-file dir))))
-
+    (find-file (expand-file-name nb-master-file-name dir))))
 
 
 ;; * Notebook agenda
@@ -59,9 +95,10 @@
   "Show org-agenda for org-files in the notebook."
   (interactive)
   (let ((org-agenda-files (mapcar
-			   (lambda (f) (expand-file-name f (projectile-project-root)))
+			   (lambda (f) (expand-file-name
+					f (projectile-project-root)))
 			   (-filter (lambda (f)
-				      (and 
+				      (and
 				       (f-ext? f "org")
 				       (not (s-contains? "#" f))))
 				    (projectile-current-project-files)))))
@@ -80,7 +117,7 @@ uncommitted changes, you will be prompted to continue."
       (when
 	  (y-or-n-p
 	   (format
-	    "Your notebook contains uncommitted changes or files:\n%s\n Continue?" output))
+	    "Your notebook contains uncommitted changes or files:\n%s\n Continue? " output))
 	(shell-command
 	 (format
 	  "git archive --format zip HEAD -o \"%s-%s.zip\""
@@ -89,7 +126,6 @@ uncommitted changes, you will be prompted to continue."
 	  (format-time-string "%Y-%m-%d-%H:%M%p")))))))
 
 ;; * Add a menu to scimax
-
 
 (easy-menu-change
  '("Scimax") "notebook"
@@ -113,25 +149,11 @@ uncommitted changes, you will be prompted to continue."
 	      ;; visibility
 	      t))
 	   projectile-known-projects)
-   ;; (sort (mapcar (lambda (x)
-   ;; 		   (vector
-   ;; 		    ;; entry
-   ;; 		    (file-name-nondirectory (substring x 0 -1))
-   ;; 		    ;; action
-   ;; 		    `(lambda ()
-   ;; 		       (interactive)
-   ;; 		       (projectile-switch-project-by-name
-   ;; 			,x))
-   ;; 		    ;; visibility
-   ;; 		    t))
-   ;; 		 projectile-known-projects)
-   ;; 	 (lambda (a b) (string< (elt a 0) (elt b 0))))
    "words"))
 
-(add-hook 'menu-bar-update-hook 'update-scimax-projects-menu)
-
-
-
+(if nb-scimax-update-menu-p
+    (add-hook 'menu-bar-update-hook 'update-scimax-projects-menu)
+  (update-scimax-projects-menu))
 
 
 ;; * The end
