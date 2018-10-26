aaee22f57b26d9b1b373e2ee9c5067d3ee19cf03
John Kitchin <jkitchin@andrew.cmu.edu>
2018-10-26 11:13:35 -0400

    add another output for windows hunspell

--- debug-windows.el
+++ debug-windows.el
@@ -38,3 +38,4 @@ package-archive: %s
 (load-file "init.el")
 (message "%s: exit status = %s" ispell-program-name (shell-command ispell-program-name))
 (message "%s" (shell-command-to-string ispell-program-name))
+(message "%s" (shell-command-to-string (format "%s -D" ispell-program-name)))
