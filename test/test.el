6ec1880ba1c7b8437ff6f3e6d5ed449331096ee3
John Kitchin <jkitchin@andrew.cmu.edu>
2018-10-26 10:14:36 -0400

    Experiment with adding a test for the spell checker program.
    This is mostly an issue on Windows, where hunspell has either crashed Emacs or
    simply doesn't work. Hopefully this test will help me track changes that affect this.

--- test/test.el
+++ test/test.el
@@ -1,3 +1,7 @@
 ;; This is a placeholder test to check if it works.
 (ert-deftest test-1 ()
   (should (equal t t)))
+
+
+(ert-deftest test-hunspell-status ()
+  (should (equal 0 (shell-command ispell-program-name))))
