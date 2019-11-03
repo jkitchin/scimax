edb16912b60581e67746c405746cb2b6691bd51a
John Kitchin <jkitchin@andrew.cmu.edu>
2019-11-03 15:44:34 -0500

    Move radio checkboxes out to a separate file.

--- scimax-org.el
+++ scimax-org.el
@@ -1423,89 +1423,6 @@ It is for commands that depend on the major mode. One example is
 ;; (add-hook 'org-mode-hook (lambda ()
 ;; 			   (scimax-src-keymap-mode +1)))
 
-;; * radio checkboxes
-(defun scimax-in-radio-list-p ()
-  "Returns radio list if in one, else nil."
-  (interactive)
-  (let* ((element (org-element-context))
-	 (radio-list (cond
-		      ;; on an item. easy.
-		      ((and (eq 'item (car element))
-			    (member
-			     ":radio"
-			     (org-element-property
-			      :attr_org
-			      (org-element-property :parent element))))
-		       (org-element-property :parent element))
-		      ;; on an item paragraph
-		      ((and (eq 'paragraph (car element))
-			    (eq 'item (car (org-element-property :parent element)))
-			    (member
-			     ":radio"
-			     (org-element-property
-			      :attr_org
-			      (org-element-property
-			       :parent
-			       (org-element-property :parent element)))))
-		       (org-element-property
-			:parent
-			(org-element-property :parent element)))
-		      ;; not on an item or item paragraph
-		      (t
-		       nil))))
-    radio-list))
-
-(defun scimax-radio-CcCc ()
-  "Hook function for C-cC-c to work in radio checklists."
-  (interactive)
-  (let ((radio-list (scimax-in-radio-list-p))
-	(p (point)))
-    (when radio-list
-      ;; clear all boxes
-      (save-excursion
-	(mapc (lambda (el)
-		(goto-char (car el))
-		(when (re-search-forward "\\[X\\]" (line-end-position) t)
-		  (replace-match "[ ]")))
-	      (org-element-property :structure radio-list))
-	;; Now figure out where to put the new X
-	(mapc (lambda (el)
-		(when (and (> p (car el))
-			   (< p (car (last el))))
-		  (goto-char (car el))
-		  (when (re-search-forward "\\[ \\]" (line-end-position) t)
-		    (replace-match "[X]"))))
-	      (org-element-property :structure radio-list)))
-      ;; return t so the hook ends I think
-      t)))
-
-(add-hook 'org-ctrl-c-ctrl-c-hook 'scimax-radio-CcCc)
-;; this works with mouse checking.
-(add-hook 'org-checkbox-statistics-hook 'scimax-radio-CcCc)
-
-(defun scimax-org-get-plain-list (name)
-  "Get the org-element representation of a plain-list named NAME."
-  (catch 'found
-    (org-element-map
-        (org-element-parse-buffer)
-        'plain-list
-      (lambda (plain-list)
-        (when
-            (string= name (org-element-property :name plain-list))
-          (throw 'found plain-list))))))
-
-
-(defun scimax-get-radio-list-value (name)
-  "Return the value of the checked item in a radio list named NAME."
-  (save-excursion
-    (loop for el in (org-element-property
-                     :structure
-                     (scimax-org-get-plain-list name))
-          if (string= (nth 4 el) "[X]")
-          return (progn
-                   (let ((item (buffer-substring (car el) (car (last el)))))
-                     (string-match "\\[X\\]\\(.*\\)$" item)
-                     (match-string-no-properties 1 item))))))
 
 
 ;; * Fixing <> fontlock in src blocks
@@ -1538,6 +1455,9 @@ This function should be added to `org-mode-hook' to make it work."
 
 (add-hook 'org-mode-hook
 	  #'scimax-fix-<>-syntax)
+(use-package scimax-org-radio-checkbox
+  :load-path scimax-dir)
+
 
 ;; * The end
 (provide 'scimax-org)
