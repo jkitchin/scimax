91d42de37e9f2423039c5f8f3c1a4f89366a2ef2
John Kitchin <johnrkitchin@gmail.com>
2020-01-16 19:35:50 -0500

    refactor the markup questions to work on issue #333.
    This version has more easily debugged functions, and an actual location to find
    them in the src. The previous version generated the functions in a way that made
    them trickier to debug.
    
    This version should fix the issue reported in #333.

--- scimax-org.el
+++ scimax-org.el
@@ -272,78 +272,152 @@ is positive, move after, and if negative, move before."
 
 
 ;; * Markup commands for org-mode
-(loop for (type beginning-marker end-marker)
-      in '((subscript "_{" "}")
-	   (superscript "^{" "}")
-	   (italics "/" "/")
-	   (bold "*" "*")
-	   (verbatim "=" "=")
-	   (code "~" "~")
-	   (underline "_" "_")
-	   (strikethrough "+" "+"))
-      do
-      (eval `(defun ,(intern (format "org-%s-region-or-point" type)) ()
-	       ,(format "%s the region, word or character at point"
-			(upcase (symbol-name type)))
-	       (interactive)
-	       (cond
-		;; We have an active region we want to apply
-		((region-active-p)
-		 (let* ((bounds (list (region-beginning) (region-end)))
-			(start (apply 'min bounds))
-			(end (apply 'max bounds))
-			(lines))
-		   (unless (memq ',type '(subscript superscript))
-		     (save-excursion
-		       (goto-char start)
-		       (unless (looking-at " \\|\\<")
-			 (backward-word)
-			 (setq start (point)))
-		       (goto-char end)
-		       (unless (looking-at " \\|\>")
-			 (forward-word)
-			 (setq end (point)))))
-		   (setq lines
-			 (s-join "\n" (mapcar
-				       (lambda (s)
-					 (if (not (string= (s-trim s) ""))
-					     (concat ,beginning-marker
-						     (s-trim s)
-						     ,end-marker)
-					   s))
-				       (split-string
-					(buffer-substring start end) "\n"))))
-		   (setf (buffer-substring start end) lines)
-		   (forward-char (length lines))))
-		;; We are on a word with no region selected
-		((thing-at-point 'word)
-		 (cond
-		  ;; beginning of a word
-		  ((looking-back " " 1)
-		   (insert ,beginning-marker)
-		   (re-search-forward "\\>")
-		   (insert ,end-marker))
-		  ;; end of a word
-		  ((looking-back "\\>" 1)
-		   (insert ,(concat beginning-marker end-marker))
-		   (backward-char ,(length end-marker)))
-		  ;; not at start or end, so we just sub/sup the character at point
-		  ((memq ',type '(subscript superscript))
-		   (insert ,beginning-marker)
-		   (forward-char ,(- (length beginning-marker) 1))
-		   (insert ,end-marker))
-		  ;; somewhere else in a word, and handled sub/sup. mark up the
-		  ;; whole word.
-		  (t
-		   (re-search-backward "\\<")
-		   (insert ,beginning-marker)
-		   (re-search-forward "\\>")
-		   (insert ,end-marker))))
-		;; not at a word or region, insert markers and put point between
-		;; them.
-		(t
-		 (insert ,(concat beginning-marker end-marker))
-		 (backward-char ,(length end-marker)))))))
+
+
+(defun org-markup-region-or-point (type beginning-marker end-marker)
+  "Apply the markup TYPE with BEGINNING-MARKER and END-MARKER to region, word or point.
+This is a generic function used to apply markups. It is mostly
+the same for the markups, but there are some special cases for
+subscripts and superscripts."
+  (cond
+   ;; We have an active region we want to apply
+   ((region-active-p)
+    (let* ((bounds (list (region-beginning) (region-end)))
+	   (start (apply 'min bounds))
+	   (end (apply 'max bounds))
+	   (lines))
+      (unless (memq type '(subscript superscript))
+	(save-excursion
+	  (goto-char start)
+	  (unless (looking-at " \\|\\<")
+	    (backward-word)
+	    (setq start (point)))
+	  (goto-char end)
+	  (unless (or (looking-at " \\|\\>")
+		      (looking-back "\\>" 1))
+	    (forward-word)
+	    (setq end (point)))))
+      (setq lines
+	    (s-join "\n" (mapcar
+			  (lambda (s)
+			    (if (not (string= (s-trim s) ""))
+				(concat beginning-marker
+					(s-trim s)
+					end-marker)
+			      s))
+			  (split-string
+			   (buffer-substring start end) "\n"))))
+      (setf (buffer-substring start end) lines)
+      (forward-char (length lines))))
+   ;; We are on a word with no region selected
+   ((thing-at-point 'word)
+    (cond
+     ;; beginning of a word
+     ((looking-back " " 1)
+      (insert beginning-marker)
+      (re-search-forward "\\>")
+      (insert end-marker))
+     ;; end of a word
+     ((looking-back "\\>" 1)
+      (insert (concat beginning-marker end-marker))
+      (backward-char (length end-marker)))
+     ;; not at start or end so we just sub/sup the character at point
+     ((memq type '(subscript superscript))
+      (insert beginning-marker)
+      (forward-char (- (length beginning-marker) 1))
+      (insert end-marker))
+     ;; somewhere else in a word and handled sub/sup. mark up the
+     ;; whole word.
+     (t
+      (re-search-backward "\\<")
+      (insert beginning-marker)
+      (re-search-forward "\\>")
+      (insert end-marker))))
+   ;; not at a word or region insert markers and put point between
+   ;; them.
+   (t
+    (insert (concat beginning-marker end-marker))
+    (backward-char (length end-marker)))))
+
+
+(defun org-italics-region-or-point ()
+  "Italicize the region, word or character at point.
+This function tries to do what you mean:
+1. If you select a region, markup the region.
+2. If in a word, markup the word.
+3. Otherwise wrap the character at point in the markup."
+  (interactive)
+  (org-markup-region-or-point 'italics "/" "/"))
+
+
+(defun org-bold-region-or-point ()
+  "Bold the region, word or character at point.
+This function tries to do what you mean:
+1. If you select a region, markup the region.
+2. If in a word, markup the word.
+3. Otherwise wrap the character at point in the markup."
+  (interactive)
+  (org-markup-region-or-point 'bold "*" "*"))
+
+
+(defun org-underline-region-or-point ()
+  "Underline the region, word or character at point.
+This function tries to do what you mean:
+1. If you select a region, markup the region.
+2. If in a word, markup the word.
+3. Otherwise wrap the character at point in the markup."
+  (interactive)
+  (org-markup-region-or-point 'underline "_" "_"))
+
+
+(defun org-code-region-or-point ()
+  "Mark the region, word or character at point as code.
+This function tries to do what you mean:
+1. If you select a region, markup the region.
+2. If in a word, markup the word.
+3. Otherwise wrap the character at point in the markup."
+  (interactive)
+  (org-markup-region-or-point 'underline "~" "~"))
+
+
+(defun org-verbatim-region-or-point ()
+  "Mark the region, word or character at point as verbatim.
+This function tries to do what you mean:
+1. If you select a region, markup the region.
+2. If in a word, markup the word.
+3. Otherwise wrap the character at point in the markup."
+  (interactive)
+  (org-markup-region-or-point 'underline "=" "="))
+
+
+(defun org-strikethrough-region-or-point ()
+  "Mark the region, word or character at point as strikethrough.
+This function tries to do what you mean:
+1. If you select a region, markup the region.
+2. If in a word, markup the word.
+3. Otherwise wrap the character at point in the markup."
+  (interactive)
+  (org-markup-region-or-point 'strikethrough "+" "+"))
+
+
+(defun org-subscript-region-or-point ()
+  "Mark the region, word or character at point as a subscript.
+This function tries to do what you mean:
+1. If you select a region, markup the region.
+2. If in a word, markup the word.
+3. Otherwise wrap the character at point in the markup."
+  (interactive)
+  (org-markup-region-or-point 'subscript "_{" "}"))
+
+
+(defun org-superscript-region-or-point ()
+  "Mark the region, word or character at point as superscript.
+This function tries to do what you mean:
+1. If you select a region, markup the region.
+2. If in a word, markup the word.
+3. Otherwise wrap the character at point in the markup."
+  (interactive)
+  (org-markup-region-or-point 'superscript "^{" "}"))
 
 
 (defun org-latex-math-region-or-point (&optional arg)
