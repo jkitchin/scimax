e82d4ed8b4740cdeedbaa0239cd1290d3dc0769e
John Kitchin <johnrkitchin@gmail.com>
2020-12-25 17:23:17 -0500

    rm commented macro code

--- scimax-utils.el
+++ scimax-utils.el
@@ -206,48 +206,6 @@ Returns whatever BODY would return."
 ;; * f-strings
 
 
-;; (defmacro f-string (fmt)
-;;   "Like `s-format' but with format fields in it.
-;; FMT is a string to be expanded against the current lexical
-;; environment. It is like what is used in `s-lex-format', but has
-;; an expanded syntax to allow format-strings. For example:
-;; ${user-full-name 20s} will be expanded to the current value of
-;; the variable `user-full-name' in a field 20 characters wide.
-;;   (let ((f (sqrt 5)))  (f-string \"${f 1.2f}\"))
-;;   will render as: 2.24
-;; This function is inspired by the f-strings in Python 3.6, which I
-;; enjoy using a lot.
-
-;; You can also try putting expressions in for formatting, e.g.:
-;;  (let ((a 11)) (f-string \"The sqrt of ${a} is ${(sqrt a) 1.2f}.\"))
-;;  will render as \"The sqrt of 11 is 3.32\".
-;; "
-;;   (let* ((matches (s-match-strings-all"${\\(?3:\\(?1:[^} ]+\\) *\\(?2:[^}]*\\)\\)}" fmt))
-;;          (agetter (cl-loop
-;; 		   for (m0 m1 m2 m3) in matches
-;; 		   collect
-;; 		   `(cons ,m3
-;; 			  ,(if (s-starts-with? "(" m3)
-;; 			       ;; This means an expression is used
-;; 			       (with-temp-buffer
-;; 				 (insert m3)
-;; 				 (goto-char (point-min))
-;; 				 (let ((expr (read (current-buffer)))
-;; 				       (fmt (s-trim (buffer-substring (point) (point-max)))))
-;; 				   `(format
-;; 				     (format "%%%s" (if (string= ,fmt "")
-;; 							(if s-lex-value-as-lisp "S" "s")
-;; 						      ,fmt))
-;; 				     ,expr)))
-
-;; 			     `(format
-;; 			       (format "%%%s" (if (string= ,m2 "")
-;; 						  (if s-lex-value-as-lisp "S" "s")
-;; 						,m2))
-;; 			       (symbol-value (intern ,m1))))))))
-
-;;     `(s-format ,fmt 'aget (list ,@agetter))))
-
 (defun f-string (fmt)
   "Like `s-format' but with format fields in it.
 FMT is a string to be expanded against the current lexical
