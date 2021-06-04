39ce4d21ef1d2aa22dd5dce2af05a84b89d1a723
John Kitchin <jkitchin@andrew.cmu.edu>
2021-06-04 09:16:34 -0400

    rm hotspots
    I don't use this, and it can all be done with bookmarks I think.

--- scimax-utils.el
+++ scimax-utils.el
@@ -5,45 +5,6 @@
 
 ;;; Code:
 
-;; * Hotspots
-(defcustom scimax-user-hotspot-commands '()
-  "A-list of hotspots to jump to in `hotspots'.
-These are shortcut to commands.
-\(\"label\" . command)"
-  :group 'scimax)
-
-(defcustom scimax-user-hotspot-locations '()
-  "A-list of hotspot locations to jump to in  `hotspots'.
-\(\"label\" . \"Path to file\").
-
-These are like bookmarks."
-  :group 'scimax
-  :type '(alist :key-type string :value-type file))
-
-
-;;;###autoload
-(defun hotspots (arg)
-  "Helm interface to hotspot locations.
-This includes user defined
-commands (`scimax-user-hotspot-commands'),
-locations (`scimax-user-hotspot-locations'), org agenda files,
-recent files and bookmarks. You can set a bookmark also."
-  (interactive "P")
-  (require 'helm-for-files)
-  (helm :sources `(((name . "Commands")
-		    (candidates . ,scimax-user-hotspot-commands)
-		    (action . (("Open" . (lambda (x) (funcall x))))))
-		   ((name . "My Locations")
-		    (candidates . ,scimax-user-hotspot-locations)
-		    (action . (("Open" . (lambda (x) (find-file x))))))
-		   ((name . "My org files")
-		    (candidates . ,org-agenda-files)
-		    (action . (("Open" . (lambda (x) (find-file x))))))
-		   helm-source-recentf
-		   helm-source-bookmarks
-		   helm-source-bookmark-set)))
-
-
 (add-to-list 'safe-local-eval-forms
 	     '(progn (require 'emacs-keybinding-command-tooltip-mode)
 		     (emacs-keybinding-command-tooltip-mode +1)))
