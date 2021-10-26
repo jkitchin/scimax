591fa1729b5dfeae4559b059d622837d0d185e1d
John Kitchin <jkitchin@andrew.cmu.edu>
2021-10-26 17:42:06 -0400

    updated org-ref setup

--- packages.el
+++ packages.el
@@ -329,40 +329,44 @@
 							 :with-toc nil
 							 :with-latex dvipng)))
 
-;; this is a git submodule
 (use-package parsebib)
 (use-package helm)
 (use-package helm-bibtex)
 (use-package ivy-bibtex)
+
+
+;; this is in a git submodule
 (use-package org-ref
   :ensure nil
   :load-path (lambda () (expand-file-name "org-ref" scimax-dir))
   :init
   (add-to-list 'load-path
 	       (expand-file-name "org-ref" scimax-dir))
+  (require 'bibtex)
   (setq bibtex-autokey-year-length 4
 	bibtex-autokey-name-year-separator "-"
 	bibtex-autokey-year-title-separator "-"
 	bibtex-autokey-titleword-separator "-"
 	bibtex-autokey-titlewords 2
 	bibtex-autokey-titlewords-stretch 1
-	bibtex-autokey-titleword-length 5
-	org-ref-bibtex-hydra-key-binding (kbd "H-b"))
-  ;; (define-key bibtex-mode-map org-ref-bibtex-hydra-key-binding 'org-ref-bibtex-hydra/body)
-  ;; (global-set-key (kbd "H-b") 'org-ref-bibtex-hydra/body)
-  )
+	bibtex-autokey-titleword-length 5)
+  (define-key bibtex-mode-map (kbd "H-b") 'org-ref-bibtex-hydra/body)
+  (define-key org-mode-map (kbd "C-c ]") 'org-ref-insert-link)
+  (define-key org-mode-map (kbd "s-[") 'org-ref-insert-link-hydra/body)
+  (require 'org-ref-ivy)
+  (require 'org-ref-arxiv)
+  (require 'org-ref-scopus)
+  (require 'org-ref-wos))
 
-(use-package org-ref-arxiv
-  :ensure nil
-  :load-path (lambda () (expand-file-name "org-ref" scimax-dir)))
 
-(use-package org-ref-scopus
+(use-package org-ref-ivy
   :ensure nil
-  :load-path (lambda () (expand-file-name "org-ref" scimax-dir)))
-
-(use-package org-ref-wos
-  :ensure nil
-  :load-path (lambda () (expand-file-name "org-ref" scimax-dir)))
+  :load-path (lambda () (expand-file-name "org-ref" scimax-dir))
+  :init (setq org-ref-insert-link-function 'org-ref-insert-link-hydra/body
+	      org-ref-insert-cite-function 'org-ref-cite-insert-ivy
+	      org-ref-insert-label-function 'org-ref-insert-label-link
+	      org-ref-insert-ref-function 'org-ref-insert-ref-link
+	      org-ref-cite-onclick-function (lambda (_) (org-ref-citation-hydra/body))))
 
 
 ;; https://github.com/bbatsov/projectile
