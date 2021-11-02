74b8d2af46ebc1eb3b78f035d91a30644974c38b
John Kitchin <jkitchin@andrew.cmu.edu>
2021-11-02 13:11:41 -0400

    big set of changes for using jupyter in scimax
    basically almost all the patches I want, except for what is defined in the
    wishlist of scimax-jupyter.org

--- scimax-jupyter.el
+++ scimax-jupyter.el
@@ -6,128 +6,354 @@
 (require 'scimax-ob)
 (require 'jupyter)
 
-;; * automatic buffer kernels
-
-;; (defvar scimax-jupyter-buffer-specific-kernel-p t
-;;   "If non-nil, use buffer-specific kernels.")
-
-
-;; (defvar scimax-jupyter-kill-kernel-on-exit t
-;;   "If non-nil kill local kernels.")
-
-
-;; (defun scimax-jupyter-initiate-session-advice (orig-func &optional session params)
-;;   "Initialize a Jupyter SESSION according to PARAMS.
-;; Also saves current session for killing later."
-;;   (when (equal session "none")
-;;     (setq session (if-let ((fname (buffer-file-name)))
-;; 		      (md5 fname)
-;; 		    "jupyter")))
-;;   ;; jrk added to store current client. This assumes only one session per file,
-;;   ;; which is not required, you can have more than one, but then they will not
-;;   ;; all be killed. you would have to manually name these though.
-;;   (setq header-line-format (format "session - %s" session))
-;;   (prog1
-;;       (setq jupyter-current-client
-;; 	    (org-babel-jupyter-initiate-session-by-key session params))
-;;     (when scimax-jupyter-kill-kernel-on-exit
-;;       (add-hook 'kill-buffer-hook 'scimax-jupyter-kill-kernel-hook nil t))))
-
-
-;; (defun scimax-jupyter-session-key-advice (orig-func params)
-;;   "Return a string that is the concatenation of the :session and :kernel PARAMS.
-;; PARAMS is the arguments alist as returned by
-;; `org-babel-get-src-block-info'.  The returned string can then be
-;; used to identify unique Jupyter Org babel sessions."
-;;   (let ((session (alist-get :session params))
-;;         (kernel (alist-get :kernel params)))
-;;     (when (equal session "none")
-;;       (setq session (if-let ((fname (buffer-file-name)))
-;; 			(md5 fname)
-;; 		      "jupyter")))
-;;     (unless (and session kernel
-;;                  (not (equal session "none")))
-;;       (error "Need a valid session and a kernel to form a key"))
-;;     (concat session "-" kernel)))
-
-
-;; (when scimax-jupyter-buffer-specific-kernel-p
-;;   (advice-add 'org-babel-jupyter-initiate-session :around 'scimax-jupyter-initiate-session-advice)
-;;   (advice-add 'org-babel-jupyter-session-key :around 'scimax-jupyter-session-key-advice))
-
-
-;; (defun scimax-jupyter-kill-kernel-hook ()
-;;   "Hook function to kill kernel when a buffer is killed."
-;;   (when jupyter-current-client
-;;     (kill-buffer jupyter-current-client)
-;;     (setq jupyter-current-client nil)))
-
+;; * automatic buffer kernels This is done by setting a session name in the org
+;; file <jps is a yasnippet that does this reliably. I decided for now it is too
+;; difficult to advise everything in jupyter to get the same result.
 
 
 ;; * scimax jupyter header defaults
 
-(setq org-babel-default-header-args:jupyter-python
-      '((:results . "value")
-	(:session . "jupyter")
-	(:kernel . "python3")
-	(:pandoc . t)
-	(:exports . "both")
-	(:cache .   "no")
-	(:noweb . "no")
-	(:hlines . "no")
-	(:tangle . "no")
-	(:eval . "never-export")))
-
-(setq org-babel-default-header-args:jupyter-julia
-      '((:results . "value")
-	(:session . "jupyter")
-	(:kernel . "julia-1.6")
-	(:pandoc . t)
-	(:exports . "both")
-	(:cache .   "no")
-	(:noweb . "no")
-	(:hlines . "no")
-	(:tangle . "no")
-	(:eval . "never-export")))
-
-
 (add-to-list 'org-babel-load-languages '(jupyter . t) t)
+
+(setq org-babel-default-header-args:jupyter-python
+      '((:results . "both")
+	(:session . "jupyter-python")
+	(:kernel . "python3")
+	(:pandoc . "t")
+	(:exports . "both")
+	(:cache .   "no")
+	(:noweb . "no")
+	(:hlines . "no")
+	(:tangle . "no")
+	(:eval . "never-export")))
+
+
+;; ** Julia
+(setq org-babel-default-header-args:jupyter-julia '((:async . "yes")
+                                                    (:session . "jupyter-julia")
+                                                    (:kernel . "julia-1.6")))
+
 
 ;; https://datatofish.com/add-julia-to-jupyter/
 ;;  for setup
 (add-to-list 'org-babel-load-languages '(julia . t) t)
 (setq inferior-julia-program-name "/usr/local/bin/julia")
 
+;; ** R setup
+
+;; https://developers.refinitiv.com/en/article-catalog/article/setup-jupyter-notebook-r
+(setq org-babel-default-header-args:jupyter-R
+      '((:results . "value")
+	(:session . "jupyter-R")
+	(:kernel . "ir")
+	(:pandoc . "t")
+	(:exports . "both")
+	(:cache .   "no")
+	(:noweb . "no")
+	(:hlines . "no")
+	(:tangle . "no")
+	(:eval . "never-export")))
+
+(require 'jupyter-R)
+
+;; ** do the languages setup
+
 (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
 
 ;; * syntax highlighting
 (add-to-list 'org-src-lang-modes '("jupyter-python" . python))
 (add-to-list 'org-src-lang-modes '("jupyter-julia" . julia))
-
-
-;; see [[nb:scimax::scimax-org-babel-ipython-upstream.el::c3312]]
-;; for more definitions.
-;; (cl-loop for (key . def) in '(("C-<return>" . org-ctrl-c-ctrl-c)
-;; 			      ("M-<return>" . (lambda ()
-;; 						(interactive)
-;; 						(scimax-ob-execute-and-next-block t)))
-;; 			      ("S-<return>" . scimax-ob-execute-and-next-block)
-
-;; 			      ("H-w" . org-babel-previous-src-block)
-;; 			      ("H-s" . org-babel-next-src-block)
-;; 			      ("H-j" . scimax-ob-jump-to-block)
-
-;; 			      ("H--" . scimax-ob-split-src-block)
-;; 			      ("H-m" . scimax-ob-merge-blocks)
-;; 			      ("H-l" . org-babel-remove-result)
-;; 			      ("H-L" . scimax-ob-clear-all-results))
-;; 	 do (jupyter-org-define-key (kbd key) def))
+(add-to-list 'org-src-lang-modes '("jupyter-R" . R))
 
 ;; * make old ipython blocks work with jupyter python
 
 (defalias 'org-babel-execute:ipython 'org-babel-execute:jupyter-python)
 (setq org-babel-default-header-args:ipython org-babel-default-header-args:jupyter-python)
 (add-to-list 'org-src-lang-modes '("ipython" . python))
+
+
+;; * Kill the kernel when the buffer is closed
+;; This is also harder than needed I think. This should kill the kernel in the current file.
+(defun scimax-jupyter-get-session ()
+  "Get the session name in the current buffer."
+  (let ((lang (car (org-babel-get-src-block-info))))
+    (or
+     (cdr
+      (assoc :session
+	     (cadr (org-babel-params-from-properties lang))))
+     (cdr (assoc :session
+		 org-babel-default-header-args:jupyter-python)))))
+
+
+(defun scimax-jupyter-org-kill-kernel ()
+  "Kill the kernel."
+  (interactive)
+  (let ((session (scimax-jupyter-get-session)))
+    (catch 'done
+      (cl-loop for buffer in (buffer-list)
+	       do
+	       (when (string-match
+		      (format "*jupyter-repl\\[.*\\]-%s*" session)
+		      (buffer-name buffer))
+		 (kill-buffer buffer)
+		 (setq header-line-format nil)
+		 (throw 'done buffer))))))
+
+
+(defun scimax-jupyter-kill-kernel-hook (&rest args)
+  (setq header-line-format
+	(format "%s running. Click here to kill it."
+		(scimax-jupyter-get-session)))
+  (local-set-key [header-line down-mouse-1]
+		 `(lambda ()
+		    (interactive)
+		    (scimax-jupyter-org-kill-kernel)))
+  (add-hook 'kill-buffer-hook 'scimax-jupyter-org-kill-kernel nil t))
+
+
+(defun scimax-jupyter-advise (&optional unadvise)
+  "Turn scimax-jupyter advice on.
+With prefix arg UNADVISE turn it off."
+  (interactive "P")
+  (if unadvise
+      (progn
+	(message "Un-advising emacs-jupyter")
+	(advice-remove 'org-babel-execute:jupyter  #'scimax-jupyter-kill-kernel-hook)
+	(advice-remove 'jupyter-org-sync-results  #'scimax-jupyter-org-sync-results)
+	(advice-remove 'jupyter-org--add-result  #'scimax-jupyter-org--add-result)
+	(advice-remove 'jupyter-org-export-block-or-pandoc #'scimax-jupyter-org-export-block-or-pandoc))
+    (advice-add 'org-babel-execute:jupyter :before #'scimax-jupyter-kill-kernel-hook)
+    (advice-add 'jupyter-org-sync-results :override #'scimax-jupyter-org-sync-results)
+    (advice-add 'jupyter-org--add-result :override #'scimax-jupyter-org--add-result)
+    (advice-add 'jupyter-org-export-block-or-pandoc :override #'scimax-jupyter-org-export-block-or-pandoc)))
+
+;; Turn on by default
+(scimax-jupyter-advise)
+
+;; * Fixing raw results
+;; in emacs-jupyter :results raw, :results drawer did not work. This function seems to do the trick.
+
+;; This function in emacs-jupyter seems to take a long time, and maybe is run
+;; asynchronously. That messes up the order of results, as faster running things
+;; come first in some process filter. This avoids this step and we do it later
+;; in `scimax-jupyter-org-sync-results'. There is probably no guarantee that
+;; this always works though.
+(defun scimax-jupyter-org-export-block-or-pandoc (type value params)
+  "Return VALUE, either converted with pandoc or in an export block.
+If PARAMS has non-nil value for key ':pandoc' and TYPE is in
+`jupyter-org-pandoc-convertable', convert the result with pandoc.
+Otherwise, wrap it in an export block."
+  (message "%S" (list type value params))
+  (jupyter-org-export-block type value))
+
+
+;; used to advise `jupyter-org-sync-results'. I do this to fix some issues liek
+;; retults drawers, etc.
+(defun scimax-jupyter-org-sync-results (req)
+  "Return the result string in org syntax for the results of REQ.
+Meant to be used as the return value of
+`org-babel-execute:jupyter'."
+  (when-let* ((results (nreverse (jupyter-org-request-results req))) 
+	      ;; Not sure why these are reversed above, it works right for some things,
+	      ;; but not right for display stuff. I wonder if there is some way
+	      ;; to tell when something is displayed.
+              (params (jupyter-org-request-block-params req))
+              (result-params (alist-get :result-params params)))
+
+    ;; There is an inconsistency when you specify :results value where printed
+    ;; outputs and the return value is provided, sometimes as a fixed width.
+    ;; That is consistent with the jupyter notebook, but not consistent with
+    ;; org-babel. Here I only return the last thing if value is selected, and do
+    ;; not require it to be fixed-width. That also isn't quite right, except in
+    ;; a drawer. 
+    (when (member "value" result-params)
+      (let ((retval (car (last results)))) 
+	(setq results
+	      (list
+	       (cond
+		((eq 'fixed-width (org-element-type retval))
+		 (org-element-property :value retval))
+		(t
+		 retval))))))
+
+    (when (member "both" result-params)
+      (let ((retval (car (last results)))) 
+	(setf (car (last results))
+	      (cond
+	       ((eq 'fixed-width (org-element-type retval))
+		(org-element-property :value retval))
+	       (t
+		retval)))))
+
+    ;; Do pandoc conversion here so the order of results is preserved.
+    (when (cdr (assoc :pandoc params))
+      (setq results
+	    (cl-loop for result in results
+		     collect
+		     (cond
+		      ((stringp result)
+		       result)
+		      ((and (listp result)
+			    (member (org-element-property :type result) jupyter-org-pandoc-convertable))
+		       (jupyter-pandoc-convert
+			(org-element-property :type result)
+			"org"
+			(org-element-property :value result)))
+		      (t
+		       result)))))
+    
+    (org-element-interpret-data
+     (cond
+      ;; This happens when a named block is a variable in another block.
+      ;; It is different than a :results silent header.
+      ((jupyter-org-request-silent-p req)
+       results)
+
+      ((member "raw" result-params)
+       results)
+
+      ;; fall through to a drawer for now.
+      (t
+       (apply #'jupyter-org-results-drawer results))))))
+
+
+
+;; This was causing a problem in emacs-jupyter for using code blocks as
+;; variables in other blocks. Commenting out the first line seems to fix it for
+;; me.
+(defun scimax-jupyter-org--add-result (req result)
+  (cond
+   ;; ((jupyter-org-request-silent-p req)
+   ;;  (unless (equal (jupyter-org-request-silent-p req) "none")
+   ;;    (message "%s" (org-element-interpret-data result))))
+   ((jupyter-org-request-async-p req)
+    (jupyter-org--clear-request-id req)
+    (jupyter-org--do-insert-result req result))
+   (t
+    (push result (jupyter-org-request-results req)))))
+
+
+
+;; I don't know where the \\ lines come from, this removes them.
+(defun scimax-rm-backslashes ()
+  "rm \\ from the end of lines."
+  (let ((result-start (org-babel-where-is-src-block-result)))
+    (save-excursion
+      (when (and result-start
+		 (goto-char result-start)
+		 (looking-at org-babel-result-regexp))
+	(while (re-search-forward "\\\\\\\\" (org-babel-result-end) t)
+	  (replace-match "")
+	  ;; I don't know why this is needed, but without, it was not finding everything.
+	  (goto-char result-start))))))
+
+(add-hook 'org-babel-after-execute-hook 'scimax-rm-backslashes)
+
+
+;; * Working with exceptions
+;;
+;; The syntax highlighting for exceptions seems broken. `jupyter-org-client' has
+;; a bunch of code that suggests this should be working, even referencing my
+;; blog post about it. This is ok for now I guess. As I recall the syntax
+;; highlighting is not persistent if you don't put the ansi codes in the buffer.
+;;
+;; this handled in generic functions that I find too difficult to trace for now.
+
+(defun scimax-jupyter-jump-to-error ()
+  "In a src block, jump to the line indicated as an error in the results.
+In a SyntaxError, there is not a traceback with a line number, so
+we handle it separately. It doesn't seem like it should be that
+way, but it is."
+  (interactive)
+  (let* ((cp (point))
+	 (location (org-babel-where-is-src-block-result))
+	 (case-fold-search t))
+    
+    (when (and location
+	       (goto-char location)
+	       (looking-at org-babel-result-regexp))
+      (cond
+       ;; Check for SyntaxError
+       ((string-match "SyntaxError:" (buffer-substring location (org-babel-result-end)))
+	(re-search-forward (rx (zero-or-more " ") "^") nil (org-babel-result-end))
+	(previous-line)
+	(let ((pattern (string-trim-left
+			(buffer-substring-no-properties
+			 (line-beginning-position) (line-end-position)))))
+	  (goto-char cp)
+	  (goto-char (org-element-property :begin (org-element-context)))
+	  (unless
+	      (search-forward pattern (org-element-property :end (org-element-context)) t)
+	    (message "No SyntaxError found like %s" pattern))))
+
+       ;; search for something like --> 21
+       (t
+	(goto-char location)
+	(re-search-forward "^-*> \\([[:digit:]]*\\)" (org-babel-result-end))
+	(save-match-data
+	  (goto-char cp)
+	  (goto-char (org-element-property :begin (org-element-context))))
+	(forward-line (string-to-number (match-string-no-properties 1))))))))
+
+
+;; * The scimax jupyter hydra
+;; customization of what is in jupyter
+;; These are more aligned with jupyter notebook I think
+
+;; I had to use pretty hydra to get nicely aligned columns here.
+(require 'pretty-hydra)
+
+(pretty-hydra-define scimax-jupyter-org-hydra (:color blue :hint nil)
+  ("Execute"
+   (("<return>" org-ctrl-c-ctrl-c "current" :color red)
+    ("C-<return>" org-ctrl-c-ctrl-c "current" :color red)
+    ("S-<return>" jupyter-org-execute-and-next-block "current and next" :color red)
+    ("M-<return>" (progn (org-ctrl-c-ctrl-c) (scimax-ob-insert-src-block t)) "current and new" )
+    ("C-M-<return>" jupyter-org-execute-subtree "subtree")
+    ("S-C-<return>" jupyter-org-restart-and-execute-to-point "restart to point")
+    ("S-M-<return>" jupyter-org-restart-kernel-execute-buffer "restart buffer"))
+
+   "Navigate"
+   (("p" org-babel-previous-src-block "previous" :color red)
+    ("P" jupyter-org-previous-busy-src-block "previous busy")
+    ("n" org-babel-next-src-block  "next" :color red)
+    ("N" jupyter-org-next-busy-src-block "next busy" :color red)
+    ("g" jupyter-org-jump-to-visible-block "jump to visible src")
+    ("G" jupyter-org-jump-to-block "jump to src block")
+    ("<tab>" org-cycle "(un)fold" :color red)
+    ("e" scimax-jupyter-jump-to-error "Jump to error"))
+
+   "Edit"
+   (("S-<up>" jupyter-org-move-src-block "move up" :color red)
+    ("S-<down>" (jupyter-org-move-src-block t) "move down" :color red)
+    ("x" jupyter-org-kill-block-and-results "kill block")
+    ("c" jupyter-org-copy-block-and-results "copy block")
+    ("o" (jupyter-org-clone-block t) "clone")
+    ("m" jupyter-org-merge-blocks "merge")
+    ("s" jupyter-org-split-src-block "split")
+    ("a" (jupyter-org-insert-src-block nil current-prefix-arg) "insert above")
+    ("b" (jupyter-org-insert-src-block t current-prefix-arg) "insert below")
+    ("l" org-babel-remove-result "clear result")
+    ("L" jupyter-org-clear-all-results "clear all results")
+    ("h" jupyter-org-edit-header "edit header"))
+
+   "Misc"
+   (("/" jupyter-org-inspect-src-block "inspect")
+    ("M-<tab>" completion-at-point "Complete")
+    
+    ("O" scimax-ob/body "scimax-ob")
+    ("q" nil "quit"))
+   
+   "Kernel"
+   (("s" org-babel-jupyter-scratch-buffer "scratch")
+    ("z" org-babel-switch-to-session "REPL")
+    ("i" jupyter-org-interrupt-kernel "interrupt")
+    ("r" (jupyter-org-with-src-block-client
+	  (jupyter-repl-restart-kernel)) "restart")
+    ("k" scimax-jupyter-org-kill-kernel "kill"))))
+
+
+(jupyter-org-define-key (kbd "<f12>") #'scimax-jupyter-org-hydra/body)
 
 (provide 'scimax-jupyter)
 
