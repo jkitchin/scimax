402fe5de1c8d9277ba7373b0ead4d9ea6b8de7a9
Josh Soref <jsoref@users.noreply.github.com>
2022-04-28 09:14:10 -0400

    spelling: macos
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- scimax-utils.el
+++ scimax-utils.el
@@ -302,8 +302,7 @@ You can also try putting expressions in for formatting, e.g.:
   "Take a screenshot and insert org link.
 screencapture starts in window capture mode. press space bar to
 toggle it to mouse select. with prefix arg, minimize emacs first.
-with double prefix arg, prompt for filename. Only works on Mac
-OSX."
+with double prefix arg, prompt for filename. Only works on macOS."
   (interactive "P")
   (when arg
     (suspend-frame))
@@ -375,7 +374,7 @@ ARG prompt for filename, else generate one. images are saved in
   "Take a screenshot and insert org link.
 with prefix arg, minimize emacs first.
 With a double prefix, prompt for the filename.
-Only works on Mac OSX."
+Only works on macOS."
   (interactive "P")
   (when arg
     (suspend-frame))
