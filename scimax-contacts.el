db187ba1f94b6d1eae9c98cc87cd4dcb7bded2a1
Josh Soref <jsoref@users.noreply.github.com>
2022-04-28 07:40:41 -0400

    spelling: ignored
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- scimax-contacts.el
+++ scimax-contacts.el
@@ -78,7 +78,7 @@ If you are in a contact heading we store a link."
 
 (defun scimax-contact-complete (&optional arg)
   "Completion function for a scimax-contact.
-Optional argument ARG is ingored."
+Optional argument ARG is ignored."
   (let* ((contacts (org-db-contacts-candidates))
 	 (contact (cdr (assoc (completing-read "Contact: " contacts) contacts))))
     (org-link-store-props
