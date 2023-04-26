e7f4a30dcb365324b912d4a2f4a4274d344e4450
John Kitchin <jkitchin@andrew.cmu.edu>
2023-04-26 09:01:06 -0400

    rm ox-clip submodule

--- packages.el
+++ packages.el
@@ -370,7 +370,6 @@
 
 (use-package ox-clip
   :ensure nil
-  :load-path (lambda () (expand-file-name "ox-clip" scimax-dir))
   :bind ("H-k" . ox-clip-formatted-copy))
 
 (use-package scimax-contacts
