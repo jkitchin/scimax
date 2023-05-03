47eca8b91779ea590d855de66dd78b33b986ad73
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-05-02 23:15:47 -0400

    spelling: empirically
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- org-mac-link.el
+++ org-mac-link.el
@@ -324,7 +324,7 @@ The links are of the form <link>::split::<name>."
 ;; the contents of the url bar, and copy it. It then uses the title of
 ;; the window as the text of the link. There is no way to grab links
 ;; from other open tabs, and further, if there is more than one window
-;; open, it is not clear which one will be used (though emperically it
+;; open, it is not clear which one will be used (though empirically it
 ;; seems that it is always the last active window).
 
 (defun org-as-mac-firefox-get-frontmost-url ()
