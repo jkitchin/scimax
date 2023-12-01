866ebd0b5c1bbaf30b617397429c049d7c7bad43
John Kitchin <jkitchin@andrew.cmu.edu>
2023-12-01 08:44:53 -0500

    comment out magithub
    See issue #486.

--- packages.el
+++ packages.el
@@ -199,8 +199,12 @@
   ("<f5>" . magit-status)
   ("C-c v t" . magit-status))
 
-(use-package magithub
-  :after magit)
+;; see https://github.com/jkitchin/scimax/issues/486 I have been having some
+;; annoying warnings about magithub, and I think I don't actually use it
+;; anymore.
+;; 
+;; (use-package magithub
+;;   :after magit)
 
 (use-package move-text
   :init (move-text-default-bindings))
